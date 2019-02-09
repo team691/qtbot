61dccdcabbf267d360ccb8b02aaf7d3aec30fbd5
Gabriel Totev <gttotev8@gmail.com>
2019-02-09 15:33:05 -0800

    Add non-persistent dynamic PWM speed controller loading

--- src/main/java/frc/team691/qtbot/Robot.java
+++ src/main/java/frc/team691/qtbot/Robot.java
@@ -1,34 +1,42 @@
 package frc.team691.qtbot;
 
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.PWMSpeedController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
-    private static final int NUM_MOTORS = 2;
+    private static final String[] SPEED_CONTROLLERS = new String[] {
+        "Spark",
+        "Talon"
+    };
 
-    SpeedController[] motors = new SpeedController[NUM_MOTORS];
+    ArrayList<PWMSpeedController> motors = new ArrayList<>();
     Joystick[] sticks;
 
     @Override
     public void robotInit() {
-        int i;
-
-        for (i = 0; i < NUM_MOTORS; i++) motors[i] = new Spark(i);
-
-        for (i = 0; i < NUM_MOTORS; i++) {
-            String mi = "motor" + i;
-            SmartDashboard.putNumber(mi, SmartDashboard.getNumber(mi, 0));
-            mi += "max";
-            SmartDashboard.putNumber(mi, SmartDashboard.getNumber(mi, 1));
+        for (String sc : SPEED_CONTROLLERS) {
+            SmartDashboard.putBoolean("add " + sc, false);
         }
+        SmartDashboard.putBoolean("remove", false);
+        SmartDashboard.putBoolean("clear", false);
     }
 
     @Override
     public void robotPeriodic() {
+    }
+
+    @Override
+    public void disabledInit() {
+    }
+
+    @Override
+    public void disabledPeriodic() {
+        updateMotors();
     }
 
     @Override
@@ -37,8 +45,8 @@ public class Robot extends TimedRobot {
     
     @Override
     public void autonomousPeriodic() {
-        for (int i = 0; i < NUM_MOTORS; i++) {
-            motors[i].set(SmartDashboard.getNumber("motor" + i, 0));
+        for (int i = 0; i < motors.size(); i++) {
+            motors.get(i).set(SmartDashboard.getNumber("motor" + i, 0));
         }
     }
 
@@ -52,22 +60,14 @@ public class Robot extends TimedRobot {
         for (int i = 0; i < sticks.length; i++) {
             int m = (int) SmartDashboard.getNumber("stick" + i, i);
             int am = Math.abs(m);
-            if (am < NUM_MOTORS) {
+            if (am < motors.size()) {
                 double t = (sticks[i].getThrottle() + 1) / 2;
                 t *= SmartDashboard.getNumber("motor" + am + "max", 1);
                 double out = t;
-                motors[am].set(out);
+                motors.get(am).set(out);
                 SmartDashboard.putNumber("motor" + am, out);
             }
         }
-    }
-
-    @Override
-    public void disabledInit() {
-    }
-
-    @Override
-    public void disabledPeriodic() {
     }
 
     @Override
@@ -76,6 +76,62 @@ public class Robot extends TimedRobot {
 
     @Override
     public void testPeriodic() {
+    }
+
+    private boolean updateMotors() {
+        boolean updated = false;
+        for (String sc : SPEED_CONTROLLERS) {
+            String sci = "add " + sc;
+            if (SmartDashboard.getBoolean(sci, false)) {
+                addMotor(sc);
+                SmartDashboard.putBoolean(sci, false);
+                updated = true;
+            }
+        }
+        if (SmartDashboard.getBoolean("remove", false)) {
+            popMotor();
+            SmartDashboard.putBoolean("remove", false);
+            updated = true;
+        }
+        if (SmartDashboard.getBoolean("clear", false)) {
+            while (!motors.isEmpty()) {
+                popMotor();
+            }
+            SmartDashboard.putBoolean("clear", false);
+            updated = true;
+        }
+        SmartDashboard.putNumber("numMotors", motors.size());
+        return updated;
+    }
+
+    private boolean addMotor(String type) {
+        int m = motors.size();
+        try {
+            Class<?> scClass = Class.forName("edu.wpi.first.wpilibj." + type);
+            Constructor<?> cst = scClass.getConstructor(Integer.TYPE);
+            motors.add((PWMSpeedController) cst.newInstance(m));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        String mi = "motor" + m;
+        SmartDashboard.putNumber(mi, 0);
+        mi += "max";
+        SmartDashboard.putNumber(mi, 1);
+        return true;
+    }
+
+    private boolean popMotor() {
+        int m = motors.size() - 1;
+        if (m < 0) return false;
+        /*
+        String mi = "motor" + m;
+        SmartDashboard.delete(mi);
+        mi += "max";
+        SmartDashboard.delete(mi);
+        */
+        motors.remove(m).close();
+        return true;
     }
 
     private boolean updateSticks() {
